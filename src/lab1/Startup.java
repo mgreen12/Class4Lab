12ec2527192ccb2e1aed4f5dc7e1804641cd0f4e
mgreen12 <mgreen12@L11707042770.wctc.msft>
2014-09-04 14:22:43 -0500

    Lab1 complete with testing startup class.

--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -9,7 +9,10 @@ public class Startup {
     
     public static void main(String[] args) {
         
-        
+        //Tests to show working inheritance.
+        AdvancedJavaCourse course1 = new AdvancedJavaCourse("Advanced Java", "112233");
+        course1.setPrerequisites("prereq test");
+        System.out.println(course1.getCourseName() + ", " + course1.getCourseNumber() + ", " + course1.getPrerequisites());
         
     }
     
